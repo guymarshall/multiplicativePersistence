e451786b3d391675f1d6c63019caf621fac65296
Guy Marshall <guymarshall9@gmail.com>
2022-04-06 20:50:56 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// get number as input
+// while number not 1 digit:
+  // convert to string
+  // split into chars
+  // convert chars into numbers
+  // multiply numbers together and return total
+  // add one to count
+
+document.getElementById('button').addEventListener('click', function() {
+  // let number = document.getElementById('number').value;
+  // let digits = number.toString().split('');
+	// console.log(digits);
+  let number = document.getElementById('number').value;
+  while (number > 10) {
+    let digits = splitStringIntoCharacters(number);
+    let sum = 1;
+    for (let i = 0; i < digits.length; i++) {
+      sum *= Number(digits[i]);
+    }
+    console.log(sum);
+    number = sum.toString();
+  }
+});
+
+function splitStringIntoCharacters(input) {
+  return input.split('');
+}
+
+let number = document.getElementById('number').value;
+while (number > 10) {
+  let digits = splitStringIntoCharacters(number);
+  let sum = 1;
+  for (let i = 0; i < digits.length; i++) {
+    sum *= Number(digits[i]);
+  }
+  console.log(sum);
+  number = sum;
+}
