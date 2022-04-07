268bb860f250f4f41c6acea48af9a8532576bf76
Guy Marshall <guymarshall9@gmail.com>
2022-04-07 13:40:54 +0100

    Add file to run from terminal

---
+++ multiplicativePersistence.js
@@ -0,0 +1,42 @@
+// let userInput = 277777788888899;
+// multiplicativePersistence(userInput);
+
+let highestSteps = {
+  'steps': 0,
+  'number': 0
+};
+
+for (let i = 0; i < 1000000; i++) {
+  // console.log(`${i}: ${multiplicativePersistence(i).steps}`);
+  if (multiplicativePersistence(i).steps > highestSteps.steps) {
+    highestSteps.steps = multiplicativePersistence(i).steps;
+    highestSteps.number = i;
+  }
+}
+
+console.log(`Highest step count: ${highestSteps.number} at ${highestSteps.steps}`);
+
+function splitStringIntoCharacters(input) {
+  return input.split('');
+}
+
+function multiplicativePersistence(userInput) {
+  let steps = 0;
+  const numbers = [];
+  while (userInput > 10) {
+    let digits = splitStringIntoCharacters(userInput.toString());
+    let sum = 1;
+    for (let i = 0; i < digits.length; i++) {
+      sum *= Number(digits[i]);
+    }
+    userInput = sum.toString();
+    numbers.push(userInput);
+
+    steps++;
+  }
+
+  return {
+    'steps': steps,
+    'numbers': numbers
+  };
+}
