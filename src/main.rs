f9a2858401dae0c6547f998f79b980c1ac7ab6ef
Guy Marshall <guy@cslgroupservices.co.uk>
2022-04-12 12:14:30 +0100

    Remove final commented JavaScript code

--- src/main.rs
+++ src/main.rs
@@ -38,18 +38,3 @@ fn main() {
 
 	println!("Highest step count: {} at {}", highest_steps_number, highest_steps_count);
 }
-
-/*
-for (let i = 77551000000; i < 1000000000000000; i++) {
-	// console.log(`${i}: ${multiplicativePersistence(i).steps}`);
-	if (multiplicativePersistence(i).steps > highestSteps.steps) {
-		highestSteps.steps = multiplicativePersistence(i).steps;
-		highestSteps.number = i;
-	}
-	if (i % 1000000 == 0) {
-		console.log(`Upto ${i} so far: ${highestSteps.number}`);
-	}
-}
-
-console.log(`Highest step count: ${highestSteps.number} at ${highestSteps.steps}`);
-*/
