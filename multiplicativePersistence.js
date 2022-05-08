27ac43667cfded940e05995cd3f83a8f6b880550
Guy Marshall <guymarshall9@gmail.com>
2022-05-08 18:39:08 +0100

    Add rust code to convert

--- multiplicativePersistence.js
+++ multiplicativePersistence.js
@@ -1,3 +1,47 @@
+/*fn product(mut input: u64) -> u64 {
+	let mut result: u64 = 1;
+	
+	// get each digit by mod instead of string conversion
+	while input > 0 {
+		result *= input % 10;
+		input /= 10;
+	}
+	result
+}
+  
+fn multiplicative_persistence(mut user_input: u64) -> u64 {
+	let mut steps: u64 = 0;
+
+	// 10 is smallest double digit number
+	while user_input >= 10 {
+		user_input = product(user_input);
+		steps += 1;
+	}
+	steps
+}
+
+fn main() {
+	// largest step count discovered = 277777788888899
+
+	let mut highest_steps_count: u64 = 0;
+	let mut highest_steps_number: u64 = 0;
+
+	let start: u64 = 5740000000000;
+	let finish: u64 = 1000000000000000;
+
+	for number in start..=finish {
+		// println!("{}: {}", number, multiplicative_persistence(number));
+		if multiplicative_persistence(number) > highest_steps_count {
+			highest_steps_count = multiplicative_persistence(number);
+			highest_steps_number = number;
+		}
+		if number % 100000000 == 0 {
+			println!("Upto {} so far: {}", number, highest_steps_number);
+		}
+	}
+
+	println!("Highest step count: {} at {}", highest_steps_number, highest_steps_count);
+}*/
 // let userInput = 277777788888899;
 // multiplicativePersistence(userInput);
 
