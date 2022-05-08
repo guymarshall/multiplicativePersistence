ca3521bf3216a3a6f1e7a100dee9c55285078bc0
Guy Marshall <guymarshall9@gmail.com>
2022-05-08 18:52:04 +0100

    Remove rust code comments

--- multiplicativePersistence.js
+++ multiplicativePersistence.js
@@ -1,14 +1,3 @@
-/*fn product(mut input: u64) -> u64 {
-	let mut result: u64 = 1;
-	
-	// get each digit by mod instead of string conversion
-	while input > 0 {
-		result *= input % 10;
-		input /= 10;
-	}
-	result
-}*/
-
 function product(input) {
 	let result = 1;
 
@@ -20,22 +9,6 @@ function product(input) {
 
 	return result;
 }
-
-/*function splitStringIntoCharacters(input) {
-	return input.split('');
-}*/
-
-/*fn multiplicative_persistence(mut user_input: u64) -> u64 {
-	let mut steps: u64 = 0;
-
-	// 10 is smallest double digit number
-	while user_input >= 10 {
-		user_input = product(user_input);
-		steps += 1;
-	}
-	steps
-}
-*/
 
 function multiplicativePersistence(userInput) {
 	let steps = 0;
