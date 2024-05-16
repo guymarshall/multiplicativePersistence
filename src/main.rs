2aa3c585f033129db2c024bed0fab51174253d51
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-05-16 18:53:12 +0100

    UPDATE: Trim whitespace

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,30 @@ mod maths;
 
 fn main() {
     let start_time: Instant = Instant::now();
-    
+
     let mut count: i32 = 10;
     let mut number: BigInt = BigInt::from(277777788888899_i64);
     let mut numbers_with_persistence_eleven: Vec<BigInt> = Vec::with_capacity(count as usize);
-    
+
     while count > 0 {
         let multiplicative_persistence: i32 = multiplicative_persistence(number.clone());
-        
+
         if multiplicative_persistence == 11 {
             println!("Numbers left: {count}");
             numbers_with_persistence_eleven.push(number.clone());
             count -= 1;
-            
+
             if contains_only_single_digit_factors(get_prime_factors(number.clone())) {
                 println!("{number}");
             }
         }
-        
+
         number += 1;
     }
-    
+
     println!("{:?}", numbers_with_persistence_eleven);
-    
+
     let end_time: Instant = Instant::now();
-    
+
     println!("Elapsed time: {:?}", end_time - start_time);
 }
