fc29a94824af2c6ed58d9a376dc22e82a7e9b9a9
Guy Marshall <guymarshall9@gmail.com>
2024-07-20 21:24:11 +0100

    UPDATE: Use string constant for number to start looking from

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,14 @@ use crate::maths::{contains_only_single_digit_factors, get_prime_factors, multip
 
 mod maths;
 
+// 277777788888899
+
 fn main() {
     let start_time: Instant = Instant::now();
 
     let mut count: i32 = 1000;
-    let mut number: BigInt = BigInt::from_str("277777788888899").unwrap();
+    const LAST_NUMBER: &str = "277779889788879";
+    let mut number: BigInt = BigInt::from_str(LAST_NUMBER).unwrap();
 
     while count > 0 {
         let multiplicative_persistence: i32 = multiplicative_persistence(number.clone());
@@ -24,6 +27,7 @@ fn main() {
             let prime_factors: Vec<BigInt> = get_prime_factors(number.clone());
             let contains_only_single_digit_factors: bool = contains_only_single_digit_factors(prime_factors);
             if contains_only_single_digit_factors {
+                println!("Number found!");
                 println!("{number}");
                 exit(0);
             }
@@ -31,6 +35,8 @@ fn main() {
 
         number += 1;
     }
+
+    println!("{}", number);
 
     let end_time: Instant = Instant::now();
 
