9f86a3dca1c31dd82c52bfb7af5f596f0731d824
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-12-28 23:49:56 +0000

    UPDATE: Parallelise code

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
 use crate::maths::{contains_only_single_digit_factors, multiplicative_persistence};
+use rayon::prelude::*;
 use std::process::exit;
 
 mod maths;
 
 // 0, 10, 25, 39, 77, 679, 6788, 68889, 2677889, 26888999, 3778888999, 277777788888899
 
-const UPDATE_FREQUENCY: i64 = 10_000_000;
+const START: i64 = 278127467000000;
+const CHUNK_SIZE: i64 = 1_000_000;
+const STEP: usize = CHUNK_SIZE as usize;
 
 fn main() {
     #[allow(clippy::infinite_iter)]
-    (277781550000000..).for_each(|number: i64| {
-        let multiplicative_persistence: i32 = multiplicative_persistence(number);
+    (START..).step_by(STEP).for_each(|first: i64| {
+        let numbers: Vec<i64> = (first..first + CHUNK_SIZE).collect();
 
-        if number % UPDATE_FREQUENCY == 0 {
-            println!("{number}");
-        }
+        println!("{first}");
 
-        if multiplicative_persistence > 10 {
-            println!(
-                "{number} has a multiplicative persistence of at least 11! Checking factors..."
-            );
+        numbers.par_iter().for_each(|number: &i64| {
+            let multiplicative_persistence: i32 = multiplicative_persistence(*number);
 
-            let factors_are_all_single_digits: bool = contains_only_single_digit_factors(number);
+            if multiplicative_persistence > 10 {
+                println!(
+                    "{number} has a multiplicative persistence of at least 11! Checking factors..."
+                );
 
-            if factors_are_all_single_digits {
-                println!("{number}");
-                exit(0);
+                let factors_are_all_single_digits: bool =
+                    contains_only_single_digit_factors(*number);
+
+                if factors_are_all_single_digits {
+                    println!("{number}");
+                    exit(0);
+                }
             }
-        }
+        });
     });
 }
