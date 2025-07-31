de49c52c3425280c18c00daf061bd92c05e0dec0
Guy Marshall <guymarshall9@gmail.com>
2025-07-31 01:48:25 +0100

    UPDATE: Save 11s to file

---
+++ src/File.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class File {
+    public static void saveToFile(String filename, BigInteger numberToSave) {
+        try (
+            FileWriter fileWriter = new FileWriter(filename, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)
+        ) {
+            bufferedWriter.write(numberToSave.toString());
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         // 0, 10, 25, 39, 77, 679, 6788, 68889, 2677889, 26888999, 3778888999, 277777788888899
 
-        final BigInteger START = new BigInteger("2");
-        final BigInteger END = new BigInteger("277777788888899");
+        final BigInteger START = new BigInteger("277780473000000");
+        final BigInteger END = new BigInteger("27777778888889900000");
 
         for (BigInteger i = START; i.compareTo(END) < 0; i = i.add(BigInteger.ONE)) {
             if (i.mod(BigInteger.valueOf(1_000_000)).equals(BigInteger.ZERO)) {
@@ -15,6 +15,8 @@ public class Main {
 
             if (multiplicativePersistence > 10) {
                 System.out.printf("%d has a multiplicative persistence of at least 11! Checking factors...%n", i);
+
+                File.saveToFile("11.txt", i);
 
                 boolean factorsAreAllSingleDigits = Maths.containsOnlySingleDigitFactors(i);
 
